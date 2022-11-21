67d75fb64af19afd60de3cdd1ee7acb8ed485227
thasup <thanachonfirst@hotmail.com>
2022-11-22 00:46:57 +0700

    Deploy using Truffle, hdwallet-provider and alchemy

--- truffle-config.js
+++ truffle-config.js
@@ -44,7 +44,10 @@
 // require('dotenv').config();
 // const { MNEMONIC, PROJECT_ID } = process.env;
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const fs = require('fs');
+const privateSecretKey = new Array(fs.readFileSync(".secret").toString());
+const alchemyProjectID = fs.readFileSync(".alchemy").toString().trim();
 
 module.exports = {
   /**
@@ -100,6 +103,15 @@ module.exports = {
       host: "127.0.0.1",     // Localhost (default: none)
       port: 8545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
+    },
+    goerli: {
+    // must be a thunk, otherwise truffle commands may hang in CI
+      provider: () =>
+          new HDWalletProvider({
+            privateKeys: privateSecretKey,
+            providerOrUrl: "https://eth-goerli.g.alchemy.com/v2/" + alchemyProjectID
+          }),
+      network_id: '5',
     },
   },
 
